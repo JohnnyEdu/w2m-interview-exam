c3ffb50490024537bf530cc6434177d92fc41b82
jbaez <johnny032295@gmail.com>
2022-09-28 00:11:34 -0300

    fix: new line character

--- src/main/resources/db/migration/h2/V1.0.0.0__scheme.sql
+++ src/main/resources/db/migration/h2/V1.0.0.0__scheme.sql
@@ -7,5 +7,5 @@ CREATE TABLE super_power ( super_power_id BIGINT PRIMARY KEY AUTO_INCREMENT, nam
 CREATE TABLE super_hero_power ( super_hero_power_id BIGINT PRIMARY KEY AUTO_INCREMENT,
  super_hero_id BIGINT NOT NULL, super_power_id BIGINT NOT NULL);
 
- ALTER TABLE super_hero_power ADD FOREIGN KEY (super_hero_id) REFERENCES super_hero(super_hero_id);
- ALTER TABLE super_hero_power ADD FOREIGN KEY (super_power_id) REFERENCES super_power(super_power_id);
+ALTER TABLE super_hero_power ADD FOREIGN KEY (super_hero_id) REFERENCES super_hero(super_hero_id);
+ALTER TABLE super_hero_power ADD FOREIGN KEY (super_power_id) REFERENCES super_power(super_power_id);
